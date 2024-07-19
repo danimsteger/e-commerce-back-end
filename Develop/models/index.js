4089f7d68be637e19ade79c6317f929c838445c3
danimsteger <daniellestegertutoring@gmail.com>
2024-07-19 16:13:54 -0400

    set up models and .env

---
+++ Develop/models/index.js
@@ -0,0 +1,45 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: "catgeory_id",
+});
+
+// Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+// Products belongToMany Tags (through ProductTag)
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: "product_tag",
+    unique: false,
+  },
+  as: "product_tags",
+});
+
+// Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: "product_tag",
+    unique: false,
+  },
+  as: "tags_product",
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
