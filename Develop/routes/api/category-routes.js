fc7132bfc101a939972bb2c16349c22d144f8ab0
danimsteger <daniellestegertutoring@gmail.com>
2024-07-20 21:59:28 -0400

    get category by id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -27,7 +27,7 @@ router.get("/:id", async (req, res) => {
       return;
     }
 
-    res.json(200).json(categoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
