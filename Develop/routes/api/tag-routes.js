860eddc71f63a220c1230110885ea7aca852c2fc
danimsteger <daniellestegertutoring@gmail.com>
2024-07-22 09:50:02 -0400

    tag routes completed

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: "products" }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -20,22 +20,25 @@ router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(re.params.id);
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: "products" }],
+    });
 
-    if (!categoryData) {
+    if (!tagData) {
       res.status(404).json({ message: "No tag found with that id!" });
       return;
     }
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
+    console.error(err);
   }
 });
 
 router.post("/", async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -45,7 +48,16 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -55,7 +67,11 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
